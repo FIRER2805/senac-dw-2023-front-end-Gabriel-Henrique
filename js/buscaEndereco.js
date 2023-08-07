095bb61a2390c8eeaef4fdc18aca5d8178c8a4a3
Gabriel <gabriel_henrique2805@hotmail.com>
2023-08-07 18:56:21 -0300

    busca de endereço por cep finalizada

--- js/buscaEndereco.js
+++ js/buscaEndereco.js
@@ -1,11 +1,29 @@
-function buscaEndereco(cep)
+// cep tesdado: 88095750
+async function buscaEndereco()
 {
-    console.log(cep);
+    let url = `https://viacep.com.br/ws/${buscaCep()}/json/`;
+    let json = await fetch(url).then(resposta => resposta.json());
+    mostraEndereco(json);
 }
 
-function buscaCep(event, form){
-    event.preventDefault();
+function buscaCep(){
     let cep = document.querySelectorAll('[type="text"]')[0].value;
     if(!cep.trim() == "" && !isNaN(cep))
-        buscaEndereco(cep);
+        return cep;
+}
+
+function mostraEndereco(json){
+    let campos = document.getElementById("endereco").children;
+    campos[0].innerHTML = `Rua: ${json.logradouro}`;
+    campos[1].innerHTML = `Bairro: ${json.bairro}`;
+    campos[2].innerHTML = `Cidade: ${json.localidade}`;
+    campos[3].innerHTML = `Estado: ${json.uf}`;
+}
+
+function limpaCampos(){
+    document.getElementById("cepTXT").value = "";
+    let campos = document.getElementById("endereco").children;
+    for(let elemento of campos){
+        elemento.innerHTML = "";
+    }
 }
