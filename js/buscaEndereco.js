45f23f64a2c4274ba72786158493f9f093c3b7b6
FIRER2805 <gabriel_henrique2805@hotmail.com>
2023-08-10 10:49:43 -0300

    finalizando o desing

--- js/buscaEndereco.js
+++ js/buscaEndereco.js
@@ -1,31 +1,33 @@
 // cep tesdado: 88095750
+const INPUTS_ENDERECO = "form.endereco input";
+
 async function buscaEndereco()
 {
     let url = `https://viacep.com.br/ws/${buscaCep()}/json/`;
     let json = await fetch(url)
         .then(resposta => resposta.json())
-        .catch(erro => alert("cep inexistente"));
+        .catch(() => alert("cep inexistente"));
     mostraEndereco(json);
 }
 
 function buscaCep(){
-    let cep = document.querySelectorAll('[type="text"]')[0].value;
+    let cep = document.querySelector("input#cepTXT").value;
     if(!cep.trim() == "" && !isNaN(cep))
         return cep;
 }
 
 function mostraEndereco(json){
-    let campos = document.getElementById("endereco").children;
-    campos[0].innerHTML = `Rua: ${json.logradouro}`;
-    campos[1].innerHTML = `Bairro: ${json.bairro}`;
-    campos[2].innerHTML = `Cidade: ${json.localidade}`;
-    campos[3].innerHTML = `Estado: ${json.uf}`;
+    let campos = document.querySelectorAll(INPUTS_ENDERECO);
+    campos[0].value = json.logradouro;
+    campos[1].value = json.bairro;
+    campos[2].value = json.localidade;
+    campos[3].value = json.uf;
 }
 
 function limpaCampos(){
     document.getElementById("cepTXT").value = "";
-    let campos = document.getElementById("endereco").children;
+    let campos = document.querySelectorAll(INPUTS_ENDERECO);
     for(let elemento of campos){
-        elemento.innerHTML = "";
+        elemento.value = "";
     }
 }
